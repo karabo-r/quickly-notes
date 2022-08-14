531f5aeef142aadc79504b38a6790e73b2045f24
Karabo <karaborabothata0@gmail.com>
2022-08-14 21:09:19 +0200

    base register user

---
+++ src/Pages/Register.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Register = () => {
+	return (
+		<div>
+			<h1>Register</h1>
+			<form>
+				<p>
+					username <input type="text" />
+				</p>
+				<p>
+					password <input type="password" />
+				</p>
+				<button>Register</button>
+			</form>
+		</div>
+	);
+};
+
+export default Register;
