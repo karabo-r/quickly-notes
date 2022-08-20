5b19daed1f1ab9c0c84b840a736dbe6ae997d770
Karabo <karaborabothata0@gmail.com>
2022-08-20 17:46:30 +0200

    card render layout added

--- src/App.js
+++ src/App.js
@@ -34,12 +34,105 @@ const App = () => {
 	};
 
 	return (
+		// <div style={{height: '100vh',display: 'flex', flexDirection: 'column'}}>
+
 		<Container>
 			<NoteScreen>
-				<div className="noteSidebar">sidebar</div>
+				<div className="noteSidebar">
+					<div className="searchBox">
+						<div className="icons">icons</div>
+						{/* <div className="search"></div> */}
+					</div>
+					<div className="noteElements">
+						<div className="noteCard">
+							<h1>Title</h1>
+							<p>content preview this is a test so hello mom</p>
+						</div>
+						<div className="noteCard">
+							<h1>Title</h1>
+							<p>content preview this is a test so hello mom</p>
+						</div>
+						<div className="noteCard">
+							<h1>Title</h1>
+							<p>content preview this is a test so hello mom</p>
+						</div>
+						<div className="noteCard">
+							<h1>Title</h1>
+							<p>content preview this is a test so hello mom</p>
+						</div>
+						<div className="noteCard">
+							<h1>Title</h1>
+							<p>content preview this is a test so hello mom</p>
+						</div>
+						<div className="noteCard">
+							<h1>Title</h1>
+							<p>content preview this is a test so hello mom</p>
+						</div>
+						<div className="noteCard">
+							<h1>Title</h1>
+							<p>content preview this is a test so hello mom</p>
+						</div>
+						<div className="noteCard">
+							<h1>Title</h1>
+							<p>content preview this is a test so hello mom</p>
+						</div>
+						<div className="noteCard">
+							<h1>Title</h1>
+							<p>content preview this is a test so hello mom</p>
+						</div>
+						<div className="noteCard">
+							<h1>Title</h1>
+							<p>content preview this is a test so hello mom</p>
+						</div>
+						<div className="noteCard">
+							<h1>Title</h1>
+							<p>content preview this is a test so hello mom</p>
+						</div>
+						<div className="noteCard">
+							<h1>Title</h1>
+							<p>content preview this is a test so hello mom</p>
+						</div>
+						<div className="noteCard">
+							<h1>Title</h1>
+							<p>content preview this is a test so hello mom</p>
+						</div>
+						<div className="noteCard">
+							<h1>Title</h1>
+							<p>content preview this is a test so hello mom</p>
+						</div>
+						<div className="noteCard">
+							<h1>Title</h1>
+							<p>content preview this is a test so hello mom</p>
+						</div>
+						<div className="noteCard">
+							<h1>Title</h1>
+							<p>content preview this is a test so hello mom</p>
+						</div>
+						<div className="noteCard">
+							<h1>Title</h1>
+							<p>content preview this is a test so hello mom</p>
+						</div>
+						<div className="noteCard">
+							<h1>Title</h1>
+							<p>content preview this is a test so hello mom</p>
+						</div>
+						<div className="noteCard">
+							<h1>Title</h1>
+							<p>content preview this is a test so hello mom</p>
+						</div>
+						<div className="noteCard">
+							<h1>Title</h1>
+							<p>content preview this is a test so hello mom</p>
+						</div>
+						<div className="noteCard">
+							<h1>Title</h1>
+							<p>content preview this is a test so hello mom</p>
+						</div>
+					</div>
+				</div>
 				<div className="noteEditor">
-					<input></input> 
-					<textarea ></textarea>
+					<input></input>
+					<textarea></textarea>
 				</div>
 			</NoteScreen>
 			<Contacts>
@@ -50,6 +143,7 @@ const App = () => {
 				</div>
 			</Contacts>
 		</Container>
+		// </div>
 	);
 };
 
@@ -61,12 +155,77 @@ const Container = styled.div`
 
 const NoteScreen = styled.div`
 	/* background-color: red; */
+	border: 1px solid gray;
 	display: grid;
-	grid-template-columns: 23% auto;
+	grid-template-columns: 22% auto;
 
 	.noteSidebar {
-		/* background-color: yellow; */
-		border-right: 1px solid;
+		/* border-right: 1px solid; */
+		display: grid;
+		grid-template-rows: 8% auto;
+		min-height: 0;
+
+		.searchBox {
+			display: flex;
+			align-items: center;
+			/* background-color: red; */
+			/* margin: auto; */
+			justify-content: center;
+
+			.icons {
+				background-color: white;
+				width: 80%;
+				height: 2rem;
+			}
+		}
+
+		.noteElements {
+			flex: 1;
+			overflow: auto;
+			/* background-color: green; */
+			padding: 1.2rem;
+			display: flex;
+			flex-direction: column;
+			min-height: 0;
+			overflow-wrap: normal;
+			/* overflow-wrap: anywhere; */
+
+			::-webkit-scrollbar {
+				width: 10px;
+				/* height: 1rem; */
+			}
+
+			/* Track */
+			::-webkit-scrollbar-track {
+				background: #f1f1f1;
+			}
+
+			/* Handle */
+			::-webkit-scrollbar-thumb {
+				background: #888;
+				border-radius: 10px;
+			}
+
+			/* Handle on hover */
+			::-webkit-scrollbar-thumb:hover {
+				background: #555;
+			}
+
+			.noteCard {
+				/* background-color: purple; */
+				height: 10rem;
+				border-radius: 5px;
+				margin-bottom: 1rem;
+				cursor: pointer;
+				padding-top: 1rem;
+				padding-left: 1rem;
+				padding-bottom: 1rem;
+
+				h1 {
+					font-size: 1.2rem;
+				}
+			}
+		}
 	}
 
 	.noteEditor {
@@ -75,20 +234,21 @@ const NoteScreen = styled.div`
 		padding-right: 3rem;
 		padding-top: 3rem;
 		display: grid;
-		grid-template-rows: 8% auto ;
+		grid-template-rows: 8% auto;
 		align-content: right;
 
-		input, textarea{
+		input,
+		textarea {
 			outline: none;
 			border: none;
 		}
 
-		input{
+		input {
 			font-size: 2rem;
 			border-bottom: 1px solid;
 		}
 
-		textarea{
+		textarea {
 			margin-top: 1rem;
 			font-size: 1rem;
 		}
@@ -103,15 +263,15 @@ const Contacts = styled.div`
 	padding-right: 3rem;
 	align-items: center;
 
-	.logo{
+	.logo {
 		font-size: 1.5rem;
 	}
-	
-	.socials{
+
+	.socials {
 		font-size: 1.2rem;
 	}
 
-	.socials a{
+	.socials a {
 		margin: 10px;
 		text-decoration: none;
 		color: black;
