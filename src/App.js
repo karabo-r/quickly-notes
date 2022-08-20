2c23ebe56a6ba83064be9f8922e976bccb7bc17f
Karabo <karaborabothata0@gmail.com>
2022-08-20 16:58:53 +0200

    inputs for noteeditor added

--- src/App.js
+++ src/App.js
@@ -37,11 +37,17 @@ const App = () => {
 		<Container>
 			<NoteScreen>
 				<div className="noteSidebar">sidebar</div>
-				<div className="noteEditor">editor</div>
+				<div className="noteEditor">
+					<input></input> 
+					<textarea ></textarea>
+				</div>
 			</NoteScreen>
 			<Contacts>
-				<div className="logo">logo</div>
-				<div className="socials">socials</div>
+				<h1 className="logo">Quickly</h1>
+				<div className="socials">
+					<a href="#">Twitter</a>
+					<a href="#">Github</a>
+				</div>
 			</Contacts>
 		</Container>
 	);
@@ -54,30 +60,62 @@ const Container = styled.div`
 `;
 
 const NoteScreen = styled.div`
-	background-color: red;
+	/* background-color: red; */
 	display: grid;
-	grid-template-columns: 25% auto;
+	grid-template-columns: 23% auto;
 
 	.noteSidebar {
-		background-color: yellow;
+		/* background-color: yellow; */
+		border-right: 1px solid;
 	}
 
 	.noteEditor {
-		background-color: purple;
+		/* padding: 3rem; */
+		padding-left: 2rem;
+		padding-right: 3rem;
+		padding-top: 3rem;
+		display: grid;
+		grid-template-rows: 8% auto ;
+		align-content: right;
+
+		input, textarea{
+			outline: none;
+			border: none;
+		}
+
+		input{
+			font-size: 2rem;
+			border-bottom: 1px solid;
+		}
+
+		textarea{
+			margin-top: 1rem;
+			font-size: 1rem;
+		}
 	}
 `;
 
 const Contacts = styled.div`
-	background-color: green;
+	/* background-color: green; */
 	display: flex;
 	justify-content: space-between;
+	padding-left: 3rem;
+	padding-right: 3rem;
+	align-items: center;
 
 	.logo{
-		background-color: blue;
+		font-size: 1.5rem;
+	}
+	
+	.socials{
+		font-size: 1.2rem;
 	}
 
-	.socials{
-		background-color: black;
+	.socials a{
+		margin: 10px;
+		text-decoration: none;
+		color: black;
+		outline: none;
 	}
 `;
 export default App;
