0296d1573e6292f5118b9ccaa7375b10a04f68b7
Karabo <karaborabothata0@gmail.com>
2022-08-20 18:35:14 +0200

    login page added

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,21 +1,79 @@
 import React from "react";
-
+import styled from "styled-components";
 const Login = () => {
 	return (
-		<div>
-			<h1>Login</h1>
-			<h3>Save your notes online</h3>
-			<form>
-				<p>
-					username <input type="text" />
-				</p>
-				<p>
-					password <input type="password" />
-				</p>
-				<button>Login</button>
-			</form>
-		</div>
+		<Container>
+			<div className="login-container">
+				<h1>Login</h1>
+				<p>Login into your account to view your notes</p>
+				<div className="inputs">
+					<input placeholder="Username" type="text"></input>
+					<br />
+					<input placeholder="Password" type="password"></input>
+				</div>
+				<button>Enter</button>
+				<div className="register-redirect">Sign Up</div>
+			</div>
+		</Container>
 	);
 };
+
+const Container = styled.div`
+	display: flex;
+	height: 100vh;
+	/* width: 50%; */
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	/* background-color: teal; */
+	/* position: relative; */
+
+	.login-container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		/* font-size: 2rem; */
+
+		height: 50%;
+		width: 50%;
+		/* background-color: teal; */
+
+		h1 {
+			font-size: 2rem;
+			margin-bottom: 2rem;
+		}
+
+		p {
+			font-size: 1rem;
+			margin-bottom: 2rem;
+		}
+
+		button{
+			margin-top: 1rem;
+			padding-left: 2rem;
+			padding-right: 2rem;
+			padding-top: 0.7rem;
+			padding-bottom: 0.7rem;
+			outline: none;
+		}
+
+		.inputs {
+			input {
+				outline: none;
+				height: 2rem;
+				margin-bottom: 1rem;
+				font-size: 1rem;
+				padding-left: 1rem;
+			}
+		}
+
+		.register-redirect {
+			position: relative;
+			/* margin-top: 1rem */
+			bottom: -3rem;
+		}
+	}
+`;
 
 export default Login;
